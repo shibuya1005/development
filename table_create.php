78c1535789f851a1adbf76f2c4a72961cc7bb30c
sharin <91249942+shibuya1005@users.noreply.github.com>
2022-02-25 20:13:21 +0900

    Update table_create.php

--- table_create.php
+++ table_create.php
@@ -1,8 +1,8 @@
 <?php 
 //MySQL(MariaDB)の専用領域にテーブル作成
 //MariaDBに接続
-$Luk = mysqli_connect( 'localhost' , 'php' , 'php' , 'php' ,);
- if( $Lnk == false ) {
+$lnk = mysqli_connect( 'localhost' , 'php' , 'php' , 'php' ,);
+ if( $lnk == false ) {
 //MariaDBに接続できなかった場合
    echo 'Connect Err<br>';
    exit;
@@ -12,7 +12,7 @@ $Luk = mysqli_connect( 'localhost' , 'php' , 'php' , 'php' ,);
 //テーブル削除のSQLを作成
 $sql = "";
 $sql .= "drop table URIAGE_Goukei\n";
-$res = mysqli_query( $Lnk , $sql ); //実行
+$res = mysqli_query( $lnk , $sql ); //実行
 if( $res == false ) {
   echo "Drop Err<br>";
 //exit;
@@ -25,7 +25,7 @@ $sql .= "CREATE TABLE URIAGE_Goukei\n";
 $sql .= "( name VARCHAR(100)\n";
 $sql .= ",  time TIMESTAMP(6)\n";
 $sql .= ")\n";
-$res = mysql_query( $Lnk , $sql ); //実行
+$res = mysql_query( $lnk , $sql ); //実行
 if( $res == false ) {
   echo "Create Err<br>";
   exit;
@@ -33,6 +33,6 @@ if( $res == false ) {
 echo "Create Ok [" . $res . "]<br>";
 
 //MariaDB切断
-mysqli_close( $Lnk );
+mysqli_close( $lnk );
 
 ?> 
